08acc9ac3f75bc2d13882c6d958db3704bf71fc7
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-12-28 21:09:25 -0300

    test(login-router): Ensure that return 400 if no password is receive

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,14 +1,27 @@
 class LoginRouter {
-  route () {
-    return { statusCode: 400 };
+  route (httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) return { statusCode: 400 };
   }
 }
+
 describe("Login Router", () => {
   test("Should return 400 if no email is receive", () => {
     const sut = new LoginRouter(); // sut - system under test
     const httpRequest = {
       body: {
         password: "any_pass",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test("Should return 400 if no password is receive", () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: "any_email@email.com",
       },
     };
     const httpResponse = sut.route(httpRequest);
