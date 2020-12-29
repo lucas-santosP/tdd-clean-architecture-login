eddba1e85618b7ca38729504a4e15828384d9710
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-12-28 23:22:00 -0300

    test(login-router): Ensure that AuthUseCase is called with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,13 +2,21 @@ import LoginRouter from "./login-router";
 import { MissingParamError } from "../helpers";
 
 function makeSut () {
-  // sut - system under test
-  return new LoginRouter();
+  class AuthUseCaseSpy {
+    auth (email, password) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy();
+  const sut = new LoginRouter(authUseCaseSpy);
+  return { authUseCaseSpy, sut };
 }
 
 describe("Login Router", () => {
   test("Should return 400 if no email is received", () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: "any_pass",
@@ -21,7 +29,7 @@ describe("Login Router", () => {
   });
 
   test("Should return 400 if no password is received", () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: "any_email@email.com",
@@ -34,17 +42,31 @@ describe("Login Router", () => {
   });
 
   test("Should return 500 if no httpRequest is received", () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpResponse = sut.route();
 
     expect(httpResponse.statusCode).toBe(500);
   });
 
-  test("Should return 500 if httpRequest has no body ", () => {
-    const sut = makeSut();
+  test("Should return 500 if httpRequest has no body", () => {
+    const { sut } = makeSut();
     const httpRequest = {};
     const httpResponse = sut.route(httpRequest);
 
     expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test("Should call AuthUseCase with correct params", () => {
+    const { sut, authUseCaseSpy } = makeSut();
+    const httpRequest = {
+      body: {
+        email: "any_email@email.com",
+        password: "any_pass",
+      },
+    };
+    sut.route(httpRequest);
+
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
   });
 });
