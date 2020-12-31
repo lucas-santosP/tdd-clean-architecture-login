1242d40ce567cf306fe06b34df752c1e3331f57e
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-12-31 16:36:22 -0300

    test(auth-usecase): Ensure that throws a MissingParamError

--- __tests__/domain/usecases/auth-usecase.test.js
+++ __tests__/domain/usecases/auth-usecase.test.js
@@ -1,7 +1,12 @@
+import { MissingParamError } from "../../../src/utils/generic-erros";
+
 class AuthUseCase {
   async auth ({ email, password }) {
-    if (!email || !password) {
-      throw new Error();
+    if (!email) {
+      throw new MissingParamError("email");
+    }
+    if (!password) {
+      throw new MissingParamError("password");
     }
     return "valid_token";
   }
@@ -12,13 +17,13 @@ describe("Auth usecase", () => {
     const sut = new AuthUseCase();
     const promise = sut.auth({ email: undefined, password: "any_pass" });
 
-    await expect(promise).rejects.toThrow();
+    await expect(promise).rejects.toThrow(new MissingParamError("email"));
   });
 
   test("Should throw an error if no password is received", async () => {
     const sut = new AuthUseCase();
     const promise = sut.auth({ email: "any_email@email.com", password: undefined });
 
-    await expect(promise).rejects.toThrow();
+    await expect(promise).rejects.toThrow(new MissingParamError("password"));
   });
 });
