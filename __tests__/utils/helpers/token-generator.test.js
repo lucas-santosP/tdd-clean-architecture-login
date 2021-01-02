8e7a83902e213108082ffbe4f39431a33f58fcb2
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-02 15:15:04 -0300

    test(token-generator): Ensure that returns a token if JWT returns token

--- __tests__/utils/helpers/token-generator.test.js
+++ __tests__/utils/helpers/token-generator.test.js
@@ -1,7 +1,21 @@
+/* eslint-disable import/first */
+jest.mock("jsonwebtoken", () => ({
+  token: "any_token",
+
+  sign (value, secretKey) {
+    this.value = value;
+    this.secretKey = secretKey;
+    return this.token;
+  },
+}));
+
+import jwt from "jsonwebtoken";
+
 function makeSut () {
   class TokenGenerator {
-    async generate () {
-      return null;
+    async generate (id) {
+      this.id = id;
+      return jwt.sign(id, "secret_key");
     }
   }
   const sut = new TokenGenerator();
@@ -9,8 +23,16 @@ function makeSut () {
 }
 
 describe("Token Generator", () => {
+  test("Should return a token if JWT returns token", async () => {
+    const { sut } = makeSut();
+    const token = await sut.generate("any_id");
+
+    expect(token).toBe(jwt.token);
+  });
+
   test("Should return null if JWT returns null", async () => {
     const { sut } = makeSut();
+    jwt.token = null;
     const token = await sut.generate("any_id");
 
     expect(token).toBe(null);
