421bc0064544fa4d08546441389fd2e45bb5b446
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-01 23:20:17 -0300

    test(email-validator): Ensure that return true if received valid email and false if invalid

---
+++ __tests__/utils/helpers/email-validator.test.js
@@ -0,0 +1,29 @@
+import validator from "validator";
+
+function makeSut () {
+  class EmailValidator {
+    validate (email) {
+      return validator.isEmail(email);
+    }
+  }
+
+  const sut = new EmailValidator();
+  return { sut };
+}
+
+describe("Email Validator", () => {
+  test("Should return true if valid email is received", async () => {
+    const { sut } = makeSut();
+    const isEmailValid = sut.validate("valid_email@email.com");
+
+    expect(isEmailValid).toBe(true);
+  });
+
+  test("Should return false if invalid email is received", async () => {
+    const { sut } = makeSut();
+    validator.isEmailValid = false;
+    const isEmailValid = sut.validate("invalid_email@email.com");
+
+    expect(isEmailValid).toBe(false);
+  });
+});
