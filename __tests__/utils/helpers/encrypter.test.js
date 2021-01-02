29e09a70f5875a4e870549cd5f1580f02cf92f78
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-02 13:46:42 -0300

    test(encrypter): Ensure that returns true if bcrypt returns true

---
+++ __tests__/utils/helpers/encrypter.test.js
@@ -0,0 +1,14 @@
+class Encrypter {
+  compare (password, hashedPassword) {
+    return true;
+  }
+}
+
+describe("Encrypter", () => {
+  test("Should return true if bcrypt returns true", async () => {
+    const sut = new Encrypter();
+    const isValid = sut.compare("any_password", "hashed_password");
+
+    expect(isValid).toBe(true);
+  });
+});
