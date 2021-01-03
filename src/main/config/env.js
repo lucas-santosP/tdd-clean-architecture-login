2f2ffc211ae437678d8b2da4f982a8daad3137fb
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-03 20:41:52 -0300

    Revert "fix: Add mongodb localhost uri"
    This reverts commit 3cb9a921669748e4f6e8ba4d2f3a274cee5cf2b8.

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017",
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/tdd-clean-api",
   tokenSecret: process.env.TOKEN_SECRET || "secret",
   port: process.env.PORT || 3000,
 };
