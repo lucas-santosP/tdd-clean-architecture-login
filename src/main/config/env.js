3cb9a921669748e4f6e8ba4d2f3a274cee5cf2b8
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-03 20:35:59 -0300

    fix: Add mongodb localhost uri

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017/tdd-clean-api",
+  mongoUrl: process.env.MONGO_URL || "mongodb://localhost:27017",
   tokenSecret: process.env.TOKEN_SECRET || "secret",
   port: process.env.PORT || 3000,
 };
