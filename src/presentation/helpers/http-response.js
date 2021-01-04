7a12ce64cec9b89949873cde1cc75c1a97053424
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-01-03 21:37:51 -0300

    feat: Add serialize-errors to normalize erros into a plain object

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,16 +1,17 @@
 import { UnauthorizedError, ServerError } from "../errors";
+import { serializeError } from "serialize-error";
 
 export default class HttpResponse {
   static badRequest (error) {
-    return { statusCode: 400, body: error };
+    return { statusCode: 400, body: serializeError(error) };
   }
 
   static serverError () {
-    return { statusCode: 500, body: new ServerError() };
+    return { statusCode: 500, body: serializeError(new ServerError()) };
   }
 
   static unauthorizedError () {
-    return { statusCode: 401, body: new UnauthorizedError() };
+    return { statusCode: 401, body: serializeError(new UnauthorizedError()) };
   }
 
   static ok (body) {
